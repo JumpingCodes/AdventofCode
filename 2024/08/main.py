ba8f4e34109ea3f6b3a731a244c85fd0f1fa7e15
Hendrik <baier.hendrik@proton.me>
2024-12-08 13:16:09 +0100

    day 8 task 2 done, dont ask me how cus i dont understand it either

--- 2024/08/main.py
+++ 2024/08/main.py
@@ -25,9 +25,11 @@ def calculate_antinodes(positions, range_x, range_y) -> set:
             if positions[i][1] == positions[j][1] and i != j:
                 difference = list(map(operator.sub, positions[i][0], positions[j][0]))
                 antinode_pos = list(map(operator.add, positions[i][0], difference))
-                if antinode_pos[0] >= 0 and antinode_pos[0] <= range_x and antinode_pos[1] >= 0 and antinode_pos[1] <= range_y:
-                    if str(antinode_pos) not in antinodes:
+                while antinode_pos[0] >= 0 and antinode_pos[0] <= range_x and antinode_pos[1] >= 0 and antinode_pos[1] <= range_y:
+                    if str(antinode_pos) not in antinodes and antinode_pos not in positions:
+                        print(antinode_pos)
                         antinodes.add(str(antinode_pos))
+                    antinode_pos = list(map(operator.add, antinode_pos, difference))
     return antinodes
 
 def main():
@@ -38,7 +40,11 @@ def main():
     antinodes = calculate_antinodes(positions, range_x, range_y)
     for line in lines:
         print(line)
-    print(len(antinodes))
+    tmp = 0
+    for pos in positions:
+        if str(pos[0]) in antinodes:
+            tmp += 1
+    print(len(antinodes) + len(positions) - tmp)
 
 
 if __name__ == "__main__":
